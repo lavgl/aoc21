3202b7b4b2634d1dd7f2929013a84c204d44dfed
Vitaly Homonov <avg.idm@gmail.com>
2022-04-12 17:46:18 +0300

    day 1 done

---
+++ days/1_1.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+const fs = std.fs;
+const fmt = std.fmt;
+const mem = std.mem;
+const print = std.debug.print;
+
+var allocator_memory: [2 * 2000 * @sizeOf(u32)]u8 = undefined;
+
+pub fn main() !void {
+    const dir = fs.cwd();
+
+    const f = try dir.openFile("1_input.txt", .{});
+    defer f.close();
+    
+    var allocator = std.heap.FixedBufferAllocator.init(allocator_memory[0..]).allocator();
+    var data = std.ArrayList(u32).init(allocator);
+    defer data.deinit();
+
+    var buf: [5]u8 = undefined;
+
+    while (try f.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try data.append(try fmt.parseInt(u32, line, 10));
+    }
+
+    var count: u32 = 0;
+    for (data.items) |depth, index| {
+        if (index == 0) continue;
+
+        if (depth > data.items[index - 1]) {
+            count += 1;
+        }
+    }
+
+    print("{}\n", .{count});
+}
---
+++ days/1_2.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+const fs = std.fs;
+const fmt = std.fmt;
+const mem = std.mem;
+const print = std.debug.print;
+
+const input_items_count = 2000;
+var allocator_memory: [2 * input_items_count * @sizeOf(u32)]u8 = undefined;
+
+pub fn main() !void {
+    const dir = fs.cwd();
+
+    const f = try dir.openFile("1_input.txt", .{});
+    defer f.close();
+    
+    var allocator = std.heap.FixedBufferAllocator.init(allocator_memory[0..]).allocator();
+    var data = std.ArrayList(u32).init(allocator);
+    defer data.deinit();
+
+    var buf: [5]u8 = undefined;
+
+    while (try f.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try data.append(try fmt.parseInt(u32, line, 10));
+    }
+
+    var count: u32 = 0;
+    for (data.items) |depth, i| {
+        if ((i < 2) or (i == data.items.len - 1)) continue;
+        const sum_a: u32 = data.items[i - 2] + data.items[i - 1] + depth;
+        const sum_b: u32 = data.items[i + 1] + depth + data.items[i - 1];
+
+        if (sum_b > sum_a) {
+            count += 1;
+        }
+    }
+
+    print("{}\n", .{count});
+}
